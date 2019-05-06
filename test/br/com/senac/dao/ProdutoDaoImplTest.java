6b4838153fd76f13195dec1de1408ce693c80f99
Alunos <Alunos@PLE110D22.alunos.sc.senac.br>
2019-05-06 20:46:59 -0300

    Merge origin/master
    Conflicts:
    	nbproject/private/private.xml

---
+++ test/br/com/senac/dao/ProdutoDaoImplTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senac.dao;
+
+import br.com.senac.model.Produto;
+import java.util.List;
+import org.hibernate.Session;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pedrão
+ */
+public class ProdutoDaoImplTest {
+    
+    private Produto produto;
+    private ProdutoDao dao;
+    private Session session;
+    
+    public ProdutoDaoImplTest() {
+    }
+
+    @Test
+    public void testPesquisaEntidadeId() {
+        System.out.println("pesquisaEntidadeId");
+        Long id = null;
+        Session session = null;
+        ProdutoDaoImpl instance = new ProdutoDaoImpl();
+        Produto expResult = null;
+        Produto result = instance.pesquisaEntidadeId(id, session);
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testListaTodos() {
+        System.out.println("listaTodos");
+        Session session = null;
+        ProdutoDaoImpl instance = new ProdutoDaoImpl();
+        List<Produto> expResult = null;
+        List<Produto> result = instance.listaTodos(session);
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testPesquisaPorNome() {
+        System.out.println("pesquisaPorNome");
+        String tipoProduto = "";
+        Session session = null;
+        ProdutoDaoImpl instance = new ProdutoDaoImpl();
+        List<Produto> expResult = null;
+        List<Produto> result = instance.pesquisaPorNome(tipoProduto, session);
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testSalvar() {
+        System.out.println("Teste Salvar");
+        dao = new ProdutoDaoImpl();
+        produto = new Produto(1L, 50, "p13", "Ultragás");
+        session = HibernateUtil.abreSessao();
+        dao.salvarOuAlterar(produto, session);
+        assertNotNull(produto.getId());
+        session.close();
+        System.out.println(produto);
+    }
+    
+}
